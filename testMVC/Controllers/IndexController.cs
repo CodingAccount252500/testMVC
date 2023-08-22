33a131aa320ecbdff51db1d08f17df9b25386416
ASUS <ASUS@DESKTOP-SV986PK>
2023-08-22 21:18:23 +0300

    com

--- testMVC/Controllers/IndexController.cs
+++ testMVC/Controllers/IndexController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using testMVC.DTO.Req;
+using testMVC.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace testMVC.Controllers
 {
 	public class IndexController : Controller
 	{
-		public IActionResult Index()
+		private readonly LMSContext _context;
+		private readonly IWebHostEnvironment _environment;
+		public IndexController(LMSContext context, IWebHostEnvironment environment) { 
+		  _context = context;
+		  _environment = environment;
+		}
+        public IActionResult Index()
 		{
 			return View();
 		}
@@ -19,8 +29,58 @@ namespace testMVC.Controllers
 		{
 			return View();
 		}
+		[HttpPost ]
+        public async Task<IActionResult> Register(string firstName ,string lastName ,string phone, string email, string password,
+			DateTime birthdate,IFormFile file)
+        {
+			if(_context.Users.Any(x=>x.Email == email))
+			{
+				return View("Error"); 
 
-		public IActionResult Login()
+            }
+			else
+			{
+				string baseEncoding = ""; string encoding = "";
+				if (file != null)
+				{
+					using (MemoryStream ms = new MemoryStream())
+					{
+						await file.CopyToAsync(ms);
+						var fileBytes = ms.ToArray();
+						baseEncoding = Convert.ToBase64String(fileBytes);
+
+						encoding = "data:"+file.ContentType+"; base64," + baseEncoding;
+					}
+				}
+                //if (file != null)
+                //{
+                //    String wRootPath = _environment.WebRootPath;
+                //    String fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+
+                //    var path1 = Path.Combine(wRootPath + "/Uploads", fileName);
+
+                //    using (var filestream = new FileStream(path1, FileMode.Create))
+                //    {
+                //        await file.CopyToAsync(filestream);
+                //    }
+                //    string path = fileName;
+
+
+                //}
+                User newPerson = new User();
+                newPerson.Name = firstName + lastName;
+				newPerson.Email = email;
+				newPerson.PhoneNumber = phone;
+				newPerson.BirthDate = birthdate;
+				newPerson.Image = encoding;
+                _context.Users.Add(newPerson);
+				_context.SaveChanges();
+
+            }	
+            return View();
+        }
+
+        public IActionResult Login()
 		{
 			return View();
 		}
